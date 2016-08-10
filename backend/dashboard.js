390c11d78147b002d23ed71d29586e7c623b7940
Jaroslav Holan <jaroslav.holan@centrum.cz>
2016-08-10 20:54:39 +0200

    Endpoint 'facebook' changed to 'photos'. Added cors.

--- backend/dashboard.js
+++ backend/dashboard.js
@@ -2,15 +2,18 @@ var express = require('express');
 var path = require('path');
 var http = require('http');
 var logger = require('morgan');
+var cors = require('cors');
 
 var FB = require('fb');
 FB.setAccessToken('523889191141898%7CEzjacE2IdM6MdNH9N8m9WSWs3GQ')
 
 var app = express();
+app.use(cors());
+app.options('*', cors());
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/facebook', function (req, res) {
+app.get('/photos', function (req, res) {
     FB.api(
         "/499635510055878/photos",
         'GET',
